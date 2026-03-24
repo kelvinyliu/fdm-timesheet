3faef81df7ee41bc6b7c100f1e02e7458f920f55
Kelvin Liu <kelvinyliu@gmail.com>
2026-03-24 12:38:14 +0000

    Added error handling

---
+++ server/src/middleware/errorHandler.js
@@ -0,0 +1,6 @@
+export default function errorHandler(err, req, res, next) {
+  const status = err.status ?? 500
+  const message = err.message ?? 'Internal server error'
+
+  res.status(status).json({ error: message })
+}

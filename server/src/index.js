ec3654f9df297ce4b5a8d3969c63603cbd4a95af
Kelvin Liu <kelvinyliu@gmail.com>
2026-03-25 15:59:31 +0000

    Implemented logger

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,14 @@
 import "dotenv/config"
 import app from './app.js'
+import logger from './logger.js'
 
 const PORT = process.env.PORT ?? 3000
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+const server = app.listen(PORT, () => {
+  logger.info({ port: PORT }, 'Server started')
+})
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM received, shutting down')
+  server.close(() => process.exit(0))
 })

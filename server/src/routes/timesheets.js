fbe42906659e7ed2cc94a503d28f61c28c72de68
Kelvin Liu <kelvinyliu@gmail.com>
2026-03-25 16:05:11 +0000

    Added router for timesheets

---
+++ server/src/routes/timesheets.js
@@ -0,0 +1,25 @@
+import { Router } from 'express'
+import auth from '../middleware/auth.js'
+import requireRole from '../middleware/requireRole.js'
+import { Role } from '../constants/roles.js'
+import {
+  listTimesheets,
+  createTimesheetHandler,
+  getTimesheet,
+  updateEntries,
+  submitTimesheet,
+  autofillTimesheet,
+} from '../controllers/timesheetController.js'
+
+const router = Router()
+
+router.use(auth)
+
+router.get('/', requireRole(Role.CONSULTANT, Role.LINE_MANAGER), listTimesheets)
+router.post('/', requireRole(Role.CONSULTANT), createTimesheetHandler)
+router.get('/:id', requireRole(Role.CONSULTANT, Role.LINE_MANAGER), getTimesheet)
+router.put('/:id/entries', requireRole(Role.CONSULTANT), updateEntries)
+router.post('/:id/submit', requireRole(Role.CONSULTANT), submitTimesheet)
+router.get('/:id/autofill', requireRole(Role.CONSULTANT), autofillTimesheet)
+
+export default router
